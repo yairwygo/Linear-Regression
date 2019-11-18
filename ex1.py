6223cfe61bc14149d4866ee249fb307f17f535ba
yairwygoda <yairwigi@gmail.com>
2019-11-18 19:40:22 +0200

    almost done

--- ex1.py
+++ ex1.py
@@ -11,10 +11,7 @@ cols = len(matrix[0])
 x=np.ones((rows,cols))
 y=np.ones((rows,1))
 
-for i in range(rows):
-    for j in range(cols-1):
-        x[i][j+1]=matrix[i][j]
-        y[i]=matrix[i][cols-1]
+
 
 print("rows : " , rows)
 print("cols : " , cols)
@@ -27,7 +24,13 @@ for i in range(rows):
         if np.var(transposedMat[j]) != 0:
             matrix[i][j]=(matrix[i][j]-np.average(transposedMat[j]))/np.sqrt(np.var(transposedMat[j]))
 
-##print (matrix)
+
+for i in range(rows):
+    for j in range(cols-1):
+        x[i][j+1]=matrix[i][j]
+        y[i]=matrix[i][cols-1]
+print (x)
+print (y)
 
 ##Q1.b
 
@@ -37,14 +40,14 @@ def h(xv,theta):
     :param theta: a list(i.e. vector) of parameters that characterize the model
     :return: the model i.e. the result of the linear function of the model
     '''
-    h = 0
+    h = 0.
     for i in range(len(theta)):
         h = h + xv[i]*theta[i]
     return h
 
 ##Q1.c
-def j(x,y,theta):
-    jCount=0
+def jErr(x,y,theta):
+    jCount=0.
     for i in range(rows):
         jCount = (h(x[i], theta)-y[i])**2
 
@@ -58,7 +61,7 @@ def gradient (x,y,theta):
     for j in range(cols):
         #i is the example
         for i in range(rows):
-            gradientList[j]=(h(x[i],theta)-y[i])*x[i][j]
+            gradientList[j]+=(h(x[i],theta)-y[i])*x[i][j]
         gradientList[j]= gradientList[j]/rows
     return gradientList
 
@@ -67,19 +70,20 @@ def gradient (x,y,theta):
 def gradienctDesent(x,y,alpha):
 
     k=0
-    numOfIteration = 10
+    numOfIteration = 20
     epsilon = 0.00001
     delta = 0.00001
-    theta0 = list(0 for i in range(cols))
-    theta1 = list(0 for i in range(cols))
-    JList = list()
+    theta0 = np.ones((cols,1))
+    theta1 = np.ones((cols,1))
+    JList = []
 
     while True:
+        grad = gradient(x,y,theta0)
         for j in range(cols):
-            for i in range(rows):
-                theta1[j]=theta0[j]-alpha*gradient(x,y,theta0)[j]
-        JList.append(j(x,y,theta0))
-        if(k > numOfIteration or np.linalg.norm(theta1-theta0) < epsilon  or np.absolute(j(x,y,theta1))-j(x,y,theta0) < delta ):
+            theta1[j]=theta0[j]-alpha*grad[j]
+
+        JList.append(jErr(x,y,theta0))
+        if(k > numOfIteration or np.linalg.norm(theta1-theta0) < epsilon  or np.absolute(jErr(x,y,theta1)-jErr(x,y,theta0)) < delta ):
             break
         theta0 = np.copy(theta1)
         k = k + 1
@@ -97,17 +101,17 @@ def miniBatch(x,y,alpha,n):
     delta = 0.00001
     theta0 = list(0 for i in range(cols))
     theta1 = list(0 for i in range(cols))
-    JList = list()
+    JList = []
     while True:
         for j in range(cols):
             for i in range(k*n,(k+1*(n-1))):
                 theta1[j] = theta0[j] - alpha * gradient(x, y, theta0)
-            JList.append(j(x, y, theta0))
-            if (k > numOfIteration or np.linalg.norm(theta1 - theta0) < epsilon or np.absolute(j(x, y, theta1)) - j(x,y,theta0) < delta):
+            JList.append(jErr(x, y, theta0))
+            if (k > numOfIteration or np.linalg.norm(theta1 - theta0) < epsilon or np.absolute(jErr(x, y, theta1) - jErr(x,y,theta0)) < delta):
                 break
             theta0 = np.copy(theta1)
             k = k + 1
-        return JList
+    return JList
 
 
 
