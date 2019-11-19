5d21a190970657f64a72405491124848993aa416
alany <alanyou73@gmail.com>
2019-11-19 18:50:43 +0200

    All Done

--- ex1.py
+++ ex1.py
@@ -1,9 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-csv_fpath = 'cancer_data.csv'
 matrix =np.genfromtxt('cancer_data.csv', delimiter=',')
-
-
 rows = len(matrix)
 cols = len(matrix[0])
 
@@ -28,10 +25,10 @@ for i in range(rows):
 for i in range(rows):
     for j in range(cols-1):
         x[i][j+1]=matrix[i][j]
-        y[i]=matrix[i][cols-1]
-print (x)
-print (y)
+    y[i]=matrix[i][cols-1]
 
+print(x)
+print(y)
 ##Q1.b
 
 def h(xv,theta):
@@ -49,7 +46,7 @@ def h(xv,theta):
 def jErr(x,y,theta):
     jCount=0.
     for i in range(rows):
-        jCount = (h(x[i], theta)-y[i])**2
+        jCount += (h(x[i], theta)-y[i])**2
 
     return jCount/(rows*2)
 
@@ -71,8 +68,8 @@ def gradienctDesent(x,y,alpha):
 
     k=0
     numOfIteration = 20
-    epsilon = 0.00001
-    delta = 0.00001
+    epsilon = 0.001
+    delta = 0.001
     theta0 = np.ones((cols,1))
     theta1 = np.ones((cols,1))
     JList = []
@@ -91,27 +88,69 @@ def gradienctDesent(x,y,alpha):
     return JList
 
 plt.plot(gradienctDesent(x,y,0.1), color = "b")
+plt.plot(gradienctDesent(x,y,0.01), color = "r")
+plt.plot(gradienctDesent(x,y,0.001), color = "g")
 plt.show()
+
 ##Q1.f
 
 def miniBatch(x,y,alpha,n):
     k = 0
     numOfIteration = 100
-    epsilon = 0.00001
-    delta = 0.00001
-    theta0 = list(0 for i in range(cols))
-    theta1 = list(0 for i in range(cols))
+    epsilon = 0.001
+    delta = 0.001
+    theta0 = np.ones((cols, 1))
+    theta1 = np.ones((cols, 1))
     JList = []
     while True:
         for j in range(cols):
-            for i in range(k*n,(k+1*(n-1))):
-                theta1[j] = theta0[j] - alpha * gradient(x, y, theta0)
-            JList.append(jErr(x, y, theta0))
-            if (k > numOfIteration or np.linalg.norm(theta1 - theta0) < epsilon or np.absolute(jErr(x, y, theta1) - jErr(x,y,theta0)) < delta):
-                break
-            theta0 = np.copy(theta1)
-            k = k + 1
+            for i in range(k*n, (k+1)*n-1):
+                new_i = i % rows
+                theta1[j] = theta1[j] - (alpha * ((h(x[new_i],theta0)-y[new_i])*x[new_i][j]))/n
+        JList.append(jErr(x, y, theta0))
+        if (k > numOfIteration or np.linalg.norm(theta1 - theta0) < epsilon or np.absolute(jErr(x, y, theta1) - jErr(x,y,theta0)) < delta):
+            break
+        theta0 = np.copy(theta1)
+        k = k + 1
     return JList
+
+
+
+plt.plot(miniBatch(x,y,0.1,50),color="b")
+plt.title("Mini batch")
+plt.show()
+
+#Q1.g
+def momentum (x,y):
+    v=np.zeros((cols,1))
+    v1=np.copy(v)
+    numOfIteration = 100
+    epsilon = 0.001
+    delta = 0.001
+    theta0=np.ones((cols,1))
+    theta1=np.copy(theta0)
+    JList = []
+    k=0
+
+    while True:
+        Err=gradient(x,y,theta0)
+
+        for j in range(cols):
+                v1[j]=0.9*v[j]+0.1*Err[j]
+                theta1[j]=theta0[j]-v1[j]
+
+        JList.append(jErr(x, y, theta0))
+        if (k > numOfIteration or np.linalg.norm(theta1 - theta0) < epsilon or np.absolute(jErr(x, y, theta1) - jErr(x,y,theta0)) < delta):
+            break
+        theta0 = np.copy(theta1)
+        v = np.copy(v1)
+        k = k + 1
+    return JList
+
+plt.plot(momentum(x,y),color="b")
+plt.title("momentum")
+plt.show()
+
 
 
 
